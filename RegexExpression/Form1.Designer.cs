2a157896ef6b1b818805b80c0fc8185fbbfc959a
Leandro <leandromfacchini@gmail.com>
2018-12-27 20:04:23 -0200

    Primeiro commit

---
+++ RegexExpression/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace RegexExpression
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Key = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnMatches = new System.Windows.Forms.Button();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.btnIsMatch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Key,
+            this.Value});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(557, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Key
+            // 
+            this.Key.HeaderText = "Chave";
+            this.Key.Name = "Key";
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "Valor";
+            this.Value.Name = "Value";
+            // 
+            // btnMatches
+            // 
+            this.btnMatches.Location = new System.Drawing.Point(12, 188);
+            this.btnMatches.Name = "btnMatches";
+            this.btnMatches.Size = new System.Drawing.Size(75, 23);
+            this.btnMatches.TabIndex = 1;
+            this.btnMatches.Text = "Matches";
+            this.btnMatches.UseVisualStyleBackColor = true;
+            this.btnMatches.Click += new System.EventHandler(this.btnMatches_Click);
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(93, 188);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(75, 23);
+            this.btnReplace.TabIndex = 2;
+            this.btnReplace.Text = "Replace";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // btnIsMatch
+            // 
+            this.btnIsMatch.Location = new System.Drawing.Point(174, 188);
+            this.btnIsMatch.Name = "btnIsMatch";
+            this.btnIsMatch.Size = new System.Drawing.Size(75, 23);
+            this.btnIsMatch.TabIndex = 3;
+            this.btnIsMatch.Text = "IsMacth";
+            this.btnIsMatch.UseVisualStyleBackColor = true;
+            this.btnIsMatch.Click += new System.EventHandler(this.btnIsMatch_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnIsMatch);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.btnMatches);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmPrincipal";
+            this.Text = "Regex Expression";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnMatches;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.Button btnIsMatch;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Key;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+    }
+}
+
---
+++ RegexExpression/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegexExpression
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMatches_Click(object sender, EventArgs e)
+        {
+            WebClient web = new WebClient();
+            var fonte = web.DownloadString("http://andresecco.com.br/2017/01/azure-tech-nights");
+
+            var rgx = new Regex(@"Dia (\d) \((\d+\/\d+\/\d+)\)");
+
+            var primeiroEncontrado = rgx.Match(fonte);
+            var encontrados = rgx.Matches(fonte);
+
+            Dictionary<int, DateTime> dic = new Dictionary<int, DateTime>();
+
+            foreach (Match item in encontrados)
+            {
+                dic.Add(int.Parse(item.Groups[1].Value), DateTime.Parse(item.Groups[2].Value));
+            }
+
+            dic.ToList().ForEach(c => dataGridView1.Rows.Add(c.Key, c.Value));
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            var rgx = new Regex(@"http://.*\d{2}\/");
+
+            var assuntosPost = rgx.Replace("http://andresecco.com.br/2017/01/azure-tech-nights/", string.Empty);
+            MessageBox.Show(assuntosPost);
+        }
+
+        private void btnIsMatch_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("É conteúdo do azure?" + (PostDesseAno("http://andresecco.com.br/2017/01/azure-tech-nights/")));
+        }
+
+        private bool PostDesseAno(string url)
+        {
+            var rgx = new Regex(@"http://.*\/azure.*");
+
+            return rgx.IsMatch(url);
+        }
+    }
+}
